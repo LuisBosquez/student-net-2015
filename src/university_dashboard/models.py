dd490614f955a86dcf47bb8980630d6aea3da980
LuisBosquez <luisdbosquez@gmail.com>
2015-03-03 23:20:04 -0600

    Cleaning template

--- src/university_dashboard/models.py
+++ src/university_dashboard/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here, yo!
 
 class University(models.Model):
-	SUPPORTED_LANGUAGES_CHOICES = (
+	SUPPORTED_LANGUAGE_CHOICES = (
 		('EN', 'English'),
 		('ES', 'Spanish'),
 		('FR', 'French')
@@ -12,19 +12,52 @@ class University(models.Model):
 	
 	name = models.CharField(max_length = 200)
 	location = models.CharField(max_length = 200)
-	language = models.CharField(max_length = 2, choices = SUPPORTED_LANGUAGES_CHOICES)
-	memberSince = models.DateTimeField('date joined', default=timezone.now())
+	language = models.CharField(max_length = 2, choices = SUPPORTED_LANGUAGE_CHOICES)
+	memberSince = models.DateTimeField('date joined', default = timezone.now())
 	
 	def __str__(self):
 		return  self.name
 
 class StudentGroup(models.Model):
-	university_id = models.ForeignKey(University)
+	STUDENT_GROUP_CATEGORY_CHOICES = (
+		('MJ', 'Major'),
+		('RA', 'Recreational Activty'),
+		('MS', 'Miscellaneous')
+		
+	)
+	
+	university = models.ForeignKey(University)
+	
 	name = models.CharField(max_length = 200)
-	size = models.IntegerField(default = 0)
-	category = models.CharField(max_length = 200, default="None specified")
+	description = models.TextField('Description')
+	category = models.CharField(max_length = 200, default="None specified", choices = STUDENT_GROUP_CATEGORY_CHOICES)	
+	
 	memberSince = models.DateTimeField('date joined', default=timezone.now())
 	
 	def __str__(self):
 		return self.name
+	
+class Student:
+	STUDENT_MAJOR_CHOICES = (
+		('CS', 'Computer Science'),
+		('EC', 'Economics'),
+		('FI', 'Finance'),
+		('MK', 'Marketing')
+	)
+	university = models.ForeignKey(University)
+	studentGroup = models.ForeignKey(StudentGroup)
+	
+	name = models.CharField(max_length = 200)
+	email = models.CharField(max_length = 200)
+	major = models.CharField(max_length = 200, choices = STUDENT_MAJOR_CHOICES)
+	
+	def __str__(self):
+		return self.name
+
+class Task:
+	studentGroup = models.ForeignKey(StudentGroup)
+	
+	
+	title = models.CharField(max_length = 200)
+	description = models.TextField('Description')
 	
